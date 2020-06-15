00cf6dc42912afdc5dd519016846a648180665d1
Lian Yi Heng <lianyiheng@scfyslpwlkjwwgc.onexmail.com>
2020-06-15 18:14:55 +0800

    nuxt 鉴权问题

---
+++ nuxt.config.js
@@ -0,0 +1,97 @@
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+
+      // 默认的meta
+      // { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+
+      // 禁止用户缩放屏幕
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    // px2 所需的静态js
+    script: [
+      {
+        src: '/flexible.js',
+        type: 'text/javascript',
+        charset: 'utf-8'
+      },
+
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    // 项目里要用的 CSS 文件
+    '@/assets/css/main.css',
+    // 项目里要使用的 SCSS 文件
+    '@/assets/css/main.scss'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '~plugins/vant.js',
+    '~/plugins/route'
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+  ],
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+    proxy: true,
+    prefix: '/api/',
+    credentials: true
+  },
+  proxy: {
+    '/api/': {
+      target: 'https://f.brandest.cc',// 这个网站是开源的可以请求到数据的
+      pathRewrite: {
+        '^/api/': '/',
+        changeOrigin: true
+      }
+    }
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    },
+    postcss: [
+      require('postcss-px2rem')({
+        remUnit: 37.5
+      })
+    ],
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="container">
+    <div>
+      <!-- <logo /> -->
+      <h4 class="title">candee_ssr_h5</h4>
+      <h2 class="subtitle">this is candee h5 ssr project</h2>
+      <div class="box">
+        <div>测试px2 是否有用</div>
+        <div>测试px2 是否有用</div>
+        <div>测试px2 是否有用</div>
+        <div>测试px2 是否有用</div>
+
+        <div class="son">
+          <van-button>tick</van-button>
+          <van-button type="warning">tick</van-button>
+        </div>
+      </div>
+      <div class="links">
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  },
+  asyncData(context) {
+    console.log(context)
+  },
+  mounted() {
+    this.getDataList()
+  },
+  methods: {
+    async getDataList() {
+      let res = await this.$axios.get('/mobile/queryAllBanner')
+      console.log(res)
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  .box {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    .son {
+      width: 375px;
+      height: 40px;
+      display: flex;
+      justify-content: space-between;
+      ::v-deep .van-button {
+        width: 100px;
+        height: 40px;
+      }
+    }
+  }
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
